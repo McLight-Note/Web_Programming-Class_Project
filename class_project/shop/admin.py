13963732ac5f2429e158949d8f8600e626b08b22
Muhammadsodiq Shukirillayev <newmclight021226@gmail.com>
2025-06-09 07:58:50 +0900

    +

--- class_project/shop/admin.py
+++ class_project/shop/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Product, CartItem, Category
 
 # Register your models here.
+admin.site.register(Product)
+admin.site.register(CartItem)
+admin.site.register(Category)
